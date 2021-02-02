60b8f2f16096f43c24f8d183172ff5aab6bf9e49
Your Name <lamisazamzam@gmail.com>
2021-02-02 23:11:06 +0600

    Double vs Triple

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,25 @@
+const firstTwo = 2;
+const secondTwo = "2";
+if(firstTwo === secondTwo){
+    console.log("is true");
+}else{
+    console.log("is false");
+}
+
+
+const numberOne = 1;
+const booleanTrue = true;
+if(numberOne == booleanTrue){
+    console.log("is true");
+}else{
+    console.log("is false");
+}
+
+
+const numberZero= 0;
+const booleanFalse = false;
+if(numberZero == booleanFalse){
+    console.log("is true");
+}else{
+    console.log("is false");
+}
