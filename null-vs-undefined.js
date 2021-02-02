1c8575baa1d991b9d33639c1727552ee879079b1
Your Name <lamisazamzam@gmail.com>
2021-02-02 20:32:52 +0600

    Null vs Undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,24 @@
+let userName;
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    //return;
+}
+const result = add(1,2);
+
+function add (num1, num2){
+    console.log(num1, num2);
+}
+const result = add(1);
+
+const userName = {name:"User", phone:1427843678};
+console.log(userName.address);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2,5,9]
+console.log(ages[11]);
+
+
+
