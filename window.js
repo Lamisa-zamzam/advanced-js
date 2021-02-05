9a25117b29f0e4657ea6e01d1e50b7e0447147f5
Your Name <lamisazamzam@gmail.com>
2021-02-05 20:37:33 +0600

    Window

--- window.js
+++ window.js
@@ -1,7 +1,10 @@
+const name = "Haity"
 function add(num1, num2){
-    const result = num1 + num2;
+    window.result = num1 + num2; //global variable result, is not recommended
+    console.log(result, name);
     return result ;
 }
 
 const sum = add(1,2);
 console.log(sum);
+console.log(name);
